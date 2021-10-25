bb1cfeb41efb8bd3c06b056539348a950caadf70
mizphses <fuminori@mizphses.com>
2021-10-25 17:28:42 +0900

    allowed_users

--- main.sql
+++ main.sql
@@ -1,3 +1,3 @@
 CREATE TABLE resource (id int, module_id macaddr, type varchar, value int, sent_at timestamp);
-CREATE TABLE devices (id int, module_id macaddr, authorized_accounts varchar);
+CREATE TABLE devices (id int, module_id macaddr, allowed_users varchar);
 CREATE TABLE users (id int, username varchar, password varchar, email varchar, display_name varchar, created_at timestamp, locked boolean, banned boolean);
